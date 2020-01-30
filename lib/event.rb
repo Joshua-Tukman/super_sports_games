94b1d8bd1ddae3d3b8fb65a7ddc857e3f680fceb
Josh Tukman <josh.tukman@gmail.com>
2020-01-30 15:48:44 -0700

    Add test for average age

--- lib/event.rb
+++ lib/event.rb
@@ -9,16 +9,24 @@ class Event
 
   def max_age
     oldest = 0
-    ages.each do |age|
+    @ages.each do |age|
       if age > oldest
         oldest = age
       end
     end
     oldest
   end
- 
+
   def min_age
     @ages.min
+  end
+  def average_age
+    total = 0
+    average = 0
+    @ages.each do |age|
+      total += age
+    end
+    average = total.to_f / @ages.length
   end
 
 end
--- test/event_test.rb
+++ test/event_test.rb
@@ -27,16 +27,16 @@ class EventTest < Minitest::Test
     event = Event.new("Curling", [24, 30, 18, 20, 41])
     assert_equal 18, event.min_age
   end
+  def test_event_has_average_age
+    event = Event.new("Curling", [24, 30, 18, 20, 41])
+    assert_equal 26.6, event.average_age
+  end
   
-
 end
 
 
 
 
-  #
-  # pry(main)> event.average_age
-  # #=> 26.6
-  #
+
   # pry(main)> event.standard_deviation_age
   # #=> 8.28
